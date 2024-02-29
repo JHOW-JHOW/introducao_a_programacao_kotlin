cfd139089bfe8108086f60ebef0d729eca2877fa
JHOW <128919491+JHOW-JHOW@users.noreply.github.com>
2024-02-29 09:49:46 -0300

    Update exercicio01.kt

--- exercicio01.kt
+++ exercicio01.kt
@@ -1,6 +1,4 @@
 EXERCICIO 1
 
 Resposta: 
-Sim, a saída desse código será os números 1, 2 e 3
-impressos nessa ordem, um abaixo do outro em forma
-crescente
+//Sim, a saída desse código será os números 1, 2 e 3 impressos nessa ordem, um abaixo do outro em forma crescente

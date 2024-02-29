fc2734550dd4eff0efe28fe9b8740eba19bdbc75
JHOW <128919491+JHOW-JHOW@users.noreply.github.com>
2024-02-29 09:47:05 -0300

    Add files via upload

---
+++ exercicio07.kt
@@ -0,0 +1,10 @@
+EXERCICIO 7
+
+Resposta:
+No lugar das chaves, foram colocados parênteses, o que 
+impediu o compilador de processar o código
+
+FORMA CORRETA
+fun main() {
+    println("How's the weather today?")
+}

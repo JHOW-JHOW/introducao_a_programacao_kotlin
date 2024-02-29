a445cea5497558f4a5b4cb5dc26971ac63ffa1d6
JHOW <128919491+JHOW-JHOW@users.noreply.github.com>
2024-02-29 09:56:38 -0300

    Update exercicio05.kt

--- exercicio05.kt
+++ exercicio05.kt
@@ -1,11 +1,9 @@
 EXERCICIO 5
-
 Resposta:
-O erro desse vez é a forma como foi escrito a função
-preintln(), como ela foi escrita de maneira errada,
-o código não funcionaria.
+//O erro desse vez é a forma como foi escrito a função preintln(), como ela foi escrita de maneira errada,
+//o código não funcionaria.
 
-FORMA CORRETA
+//FORMA CORRETA
 fun main() {
     println("There is a chance of snow")
 }

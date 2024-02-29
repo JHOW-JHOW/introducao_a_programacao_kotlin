ad5a291c50f01000b7cfd10c108c977bd994b951
JHOW <128919491+JHOW-JHOW@users.noreply.github.com>
2024-02-29 09:55:22 -0300

    Update exercicio04.kt

--- exercicio04.kt
+++ exercicio04.kt
@@ -1,10 +1,9 @@
 EXERCICIO 4
+//Resposta:
+//O erro desse código é a falta da chave de fechamento, que deveria vir após o final do body,
+//alinhado com a fun.
 
-Resposta:
-O erro desse código é a falta da chave de fechamento,
-que deveria vir após o final do body, alinhado com a fun.
-
-FORMA CORRETA
+//FORMA CORRETA
 fun main() {
     println("Tomorrow is rainy")
 }

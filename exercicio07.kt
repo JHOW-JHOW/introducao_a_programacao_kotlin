d09e62a560cc67b469349af7f2c0b88bc210ba83
JHOW <128919491+JHOW-JHOW@users.noreply.github.com>
2024-02-29 09:59:50 -0300

    Update exercicio07.kt

--- exercicio07.kt
+++ exercicio07.kt
@@ -1,10 +1,8 @@
 EXERCICIO 7
+//Resposta:
+//No lugar das chaves, foram colocados parênteses, o que  impediu o compilador de processar o código
 
-Resposta:
-No lugar das chaves, foram colocados parênteses, o que 
-impediu o compilador de processar o código
-
-FORMA CORRETA
+//FORMA CORRETA
 fun main() {
     println("How's the weather today?")
 }
